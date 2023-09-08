a7db7276841867252bfbe383d3314d316d30f519
Michie98 <mike98ndech@gmail.com>
2023-09-08 15:33:30 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- * main . A programm that prints a libe with put function
- * Return:0
+ * main - A programm that prints a libe with put function
+ * Return:0 (Success)
  */
 int main(void)
 {
