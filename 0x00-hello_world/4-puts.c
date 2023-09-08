08d87f87c14d7f76ab7931a2601fd6e3b461ee39
Michie98 <mike98ndech@gmail.com>
2023-09-08 15:48:29 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - A programm that prints a libe with put function
- * Return:0 (Success)
+ * main - A programme that uses put function
+ * Return: 0 (Success)
  */
 int main(void)
 {
-puts("/"Programming is like building a multilingual puzzle");
+puts("/"Programming is like building a multilingual puzzle")
 return (0);
 }
