aa5f31bb93a39e00a7cad0d0435f567a588fb31b
Michie98 <mike98ndech@gmail.com>
2023-09-09 02:20:21 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,11 @@
 #include <unistd.h>
 /**
  * main - Entry point
- * Return: Always 0 success
+ * Return: Always 1 (Success)
  */
 
 int main(void)
 {
-    char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    write(2, message, 59); // 59 is the length of the message
-
-    return 1;
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
 }
