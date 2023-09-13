6adf4ec4a17d39d826ca13f7d1432249e9c9bfdd
Michie98 <mike98ndech@gmail.com>
2023-09-13 12:32:30 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,27 @@
+/*
+* File: 2-print_alphabet_x10.c
+* Author: Michael Wagura
+*/
+
+#include "main.h"
+
+/**
+* print_alphabet_x10 - prints alphabet in lower case 10 times
+*
+* Return: Always 0.
+*/
+void print_alphabet_x10(void)
+{
+char l;
+
+int i;
+
+for (i = 0; i < 10; i++)
+{
+for (l = 'a'; l <= 'z'; l++)
+{
+putchar(l);
+}
+putchar('\n');
+}
+}
