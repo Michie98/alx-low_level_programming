90ef77003f6c3a9f20b1bb8d0ccd1646b0937c03
Michie98 <mike98ndech@gmail.com>
2023-09-13 12:39:49 -0700

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+/*
+* File: 3-islower.c
+* Author: Michael Wagura
+*/
+
+#include "main.h"
+
+/**
+* _islower - checks if lower case or not.
+* @c: argument to be checked
+* Return: 1 if lower, 0 if not
+*/
+int _islower(int c)
+{
+if (c >= 'a' && c <= 'z')
+return (1);
+else
+return (0);
+}
