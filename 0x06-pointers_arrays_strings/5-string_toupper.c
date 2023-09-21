fa2a1723569c9f6a3404dde159276546eed79711
Michie98 <mike98ndech@gmail.com>
2023-09-21 15:45:53 -0700

    5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+*  string_toupper -  changes string to upper
+*  @s: char arg
+*  Return: char
+*/
+
+char *string_toupper(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+		{
+			s[i] = s[i] - 32;
+		}
+	}
+	return (s);
+}
