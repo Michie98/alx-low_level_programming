32ca8b8f5e0254c191ed4db25e8ea9d8c1451caa
Michie98 <mike98ndech@gmail.com>
2023-09-25 14:02:44 -0700

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strchr - string character
+ * @s - char arg
+ * @c - char arg
+ * Return: char
+ */
+
+char *_strchr(char *s, char c);
+{
+	while (*s)
+	{
+		if (*s != c)
+		{
+			s++;
+		}
+		else
+		{
+			return (s);
+		}
+	}
+	if (c == '\0' )
+	{
+		return (s);
+	}
+	
+	return (NULL);
+}
+		
+
