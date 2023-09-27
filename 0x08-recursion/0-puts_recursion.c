de351274a0a9f969ec0fb1d8c35e495aa62ad1f4
Michie98 <mike98ndech@gmail.com>
2023-09-27 09:36:56 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,7 +7,7 @@
  */
 void _puts_recursion(char *s)
 {
-    if (*s == '\0')
+   if (*s == '\0')
    {
         _putchar('\n');
         return;
@@ -16,4 +16,3 @@ void _puts_recursion(char *s)
     _putchar(*s);
     _puts_recursion(s + 1);
 }
-
