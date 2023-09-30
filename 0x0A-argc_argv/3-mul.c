a63eb4415677d0c2e08bb117791ce31701772f39
Michie98 <mike98ndech@gmail.com>
2023-09-29 18:21:45 -0700

    3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h> /* contains atoi which converts string arg to int*/
+
+/**
+*  main - prints its name, followed by a new line.
+*  @argc: int arg
+*  @argv: char arg
+*  Return: int
+*/
+
+int main(int argc, char **argv)
+{
+	int i, j;
+
+	if (argc < 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	i = atoi(argv[1]);
+
+	j = atoi(argv[2]);
+
+	printf("%d\n", i * j);
+
+	return (0);
+}
